3331664764f123437d13e0869c42ae0a4ea4126c
Varsha Selvaraju <varshavsl.30@gmail.com>
2026-01-08 13:56:09 +0530

    moved jwt secret to yml file

--- src/main/java/com/example/expense_approval/security/JwtService.java
+++ src/main/java/com/example/expense_approval/security/JwtService.java
@@ -2,6 +2,8 @@ package com.example.expense_approval.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+//import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -10,14 +12,23 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final Key key = Keys.hmacShaKeyFor("supersecretkey123456789supersecret".getBytes());
+    private final Key key;
+    private final long expiry;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiry}") long expiry
+    ) {
+        this.expiry = expiry;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String username, String role) {
 
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role)
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .setExpiration(new Date(System.currentTimeMillis() + expiry))
                 .signWith(key)
                 .compact();
     }
